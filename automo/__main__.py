9fb28b9ac9ed36328a1c0736306524dcc4d0bc8e
Ali Aafee <ali.aafee@gmail.com>
2017-06-09 15:12:53 +0500

    Changes to database schema

--- automo/__main__.py
+++ automo/__main__.py
@@ -2,7 +2,7 @@
 AutoMO
 ------
 
-Program to automate the tedious job of hand writing prescriptions everyday
+Program to automate the tedious paper work of MOs
 
 Copyright (C) 2017 Ali Aafee
 """
@@ -11,7 +11,7 @@ import getopt
 import wx
 
 from _version import __version__
-from mainframe import MainFrame
+#from mainframe import MainFrame
 
 import database
 
@@ -52,11 +52,136 @@ def usage():
 
 def start(uri, debug):
     """ starts db session at the given db uri """
-    database.StartEngine(uri, debug)
+    database.StartEngine(uri, debug, __version__)
+
+    db_test()
 
     app = MoPresc()
 
     app.MainLoop()
+
+
+def db_test():
+    """ test code to check database functionality """
+    print "Testing Database"
+    session = database.Session()
+
+    print "Add beds"
+    for x in range(10):
+        new_bed = database.Bed(
+            name="SW {0}".format(x)
+        )
+        session.add(new_bed)
+    session.commit()
+
+    print "Add doctor"
+    doc = database.Doctor(
+        name="Dr. Ali Aafee",
+        pmr_no="PMR-001"
+    )
+    session.add(doc)
+    session.commit()
+
+    print "Add drugs"
+    drg = database.Drug(name="Ceftriaxone 1g")
+    session.add(drg)
+    session.commit()
+    drg = database.Drug(name="Metronidazole 500mg")
+    session.add(drg)
+    session.commit()
+
+    print "Add dxs"
+    new_dx = database.Diagnosis(icd_code="10.1", name="Acute Appendicitis")
+    session.add(new_dx)
+    new_dx = database.Diagnosis(icd_code="10.5", name="Acute Pancreatitis")
+    session.add(new_dx)
+    session.commit()
+
+    print "Add Patient"
+    new_pt = database.Patient(
+        name="Ali Aafee",
+        age="30",
+        sex="M"
+    )
+    session.add(new_pt)
+    session.commit()
+    print new_pt.id
+
+    import datetime
+    pre_admission = database.Admission(
+        patient_id=new_pt.id,
+        discharged_bed_id=2,
+        admitted_date=datetime.date(2017, 5, 8),
+        discharged_date=datetime.date(2017, 5, 18),
+        admission_notes="Pain abdomen x 10 days",
+        admitting_doctor_id=1
+    )
+    session.add(pre_admission)
+    session.commit()
+    pre_admission_diagnosis = database.AdmissionDiagnosis(
+        admission_id=pre_admission.id,
+        diagnosis_id=2,
+        date=datetime.date(2017, 6, 8)
+    )
+    session.add(pre_admission_diagnosis)
+    session.commit()
+    med = database.DailyRx(
+        admission_id=pre_admission.id,
+        drug_id=1,
+        drug_order="IV BD x 10 days",
+        active=True
+    )
+    session.add(med)
+    session.commit()
+    med = database.DailyRx(
+        admission_id=pre_admission.id,
+        drug_id=2,
+        drug_order="IV TDS x 10 days",
+        active=True
+    )
+    session.add(med)
+    session.commit()
+
+    new_admission = database.Admission(
+        patient_id=new_pt.id,
+        bed_id=1,
+        admitted_date=datetime.date(2017, 6, 8),
+        discharged_date=None,
+        admission_notes="Pain abdomen x 10 days",
+        admitting_doctor_id=1
+    )
+    session.add(new_admission)
+    session.commit()
+    new_admission_diagnosis = database.AdmissionDiagnosis(
+        admission_id=new_admission.id,
+        diagnosis_id=1,
+        date=datetime.date(2017, 6, 8)
+    )
+    session.add(new_admission_diagnosis)
+    session.commit()
+    med = database.DischargeRx(
+        admission_id=new_admission.id,
+        drug_id=1,
+        drug_order="IV BD x 10 days",
+        active=True
+    )
+    session.add(med)
+    session.commit()
+    med = database.DischargeRx(
+        admission_id=new_admission.id,
+        drug_id=2,
+        drug_order="IV TDS x 10 days",
+        active=True
+    )
+    session.add(med)
+    session.commit()
+
+    p = new_pt
+
+    import code
+    code.interact(local=dict(globals(), **locals()))
+
+    exit()
 
 
 def main(argv):
--- automo/database.py
+++ automo/database.py
@@ -3,7 +3,7 @@ SQLalchemy database schema for auto mo.
 """
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy import Integer, String, ForeignKey, Boolean
+from sqlalchemy import Integer, String, ForeignKey, Boolean, Date, Text
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
@@ -28,17 +28,77 @@ class Patient(Base):
     national_id_no = Column(String(10))
     name = Column(String(250))
     age = Column(String(250))
+    date_of_birth = Column(Date())
     sex = Column(String(1))
-    bed_no = Column(String(5))
-    diagnosis = Column(String(250))
-    rxs = relationship("Rx", back_populates="patient", cascade="all, delete, delete-orphan")
+    admissions = relationship("Admission", back_populates="patient",
+                              cascade="all, delete, delete-orphan")
+    #bed_no = Column(String(5))
+    #diagnosis = Column(String(250))
+    #rxs = relationship("Rx", back_populates="patient", cascade="all, delete, delete-orphan")
     active = Column(Boolean)
 
 
-class Rx(Base):
+class Admission(Base):
     patient_id = Column(Integer, ForeignKey('patient.id'))
-    patient = relationship("Patient", back_populates="rxs")
-    drug_name = Column(String(250))
+    patient = relationship("Patient", back_populates="admissions")
+
+    bed_id = Column(Integer, ForeignKey('bed.id'))
+    bed = relationship("Bed", back_populates="admission")
+
+    discharged_bed_id = Column(Integer)
+
+    admitted_date = Column(Date())
+    discharged_date = Column(Date())
+
+    admitting_doctor_id = Column(Integer, ForeignKey('doctor.id'))
+    admitting_doctor = relationship("Doctor", back_populates="admissions")
+
+    diagnoses = relationship("AdmissionDiagnosis")
+
+    admission_notes = Column(Text())
+    progress_notes = Column(Text())
+    discharge_advice = Column(Text())
+
+    daily_rxs = relationship("DailyRx")
+
+    discharge_rxs = relationship("DischargeRx")
+
+
+class Bed(Base):
+    name = Column(String(250))
+    admission = relationship("Admission", uselist=False, back_populates="bed")
+
+
+class AdmissionDiagnosis(Base):
+    admission_id = Column(Integer, ForeignKey('admission.id'))
+
+    diagnosis_id = Column(Integer, ForeignKey('diagnosis.id'))
+    diagnosis = relationship("Diagnosis")
+
+    date = Column(Date())
+
+    comment = Column(Text())
+
+
+class Diagnosis(Base):
+    icd_code = Column(String(250))
+    name = Column(String(250))
+
+
+class DischargeRx(Base):
+    admission_id = Column(Integer, ForeignKey('admission.id'))
+    #admission = relationship("Admission", back_populates="discharge_rxs")
+    drug_id = Column(Integer, ForeignKey('drug.id'))
+    drug = relationship("Drug")
+    drug_order = Column(String(250))
+    active = Column(Boolean)
+
+
+class DailyRx(Base):
+    admission_id = Column(Integer, ForeignKey('admission.id'))
+    #admission = relationship("Admission", back_populates="daily_rxs")
+    drug_id = Column(Integer, ForeignKey('drug.id'))
+    drug = relationship("Drug")
     drug_order = Column(String(250))
     active = Column(Boolean)
 
@@ -47,12 +107,10 @@ class Drug(Base):
     name = Column(String(250))
 
 
-class Diagnosis(Base):
-    name = Column(String(250))
-
-
 class Doctor(Base):
     name = Column(String(250))
+    pmr_no = Column(String(250))
+    admissions = relationship("Admission", back_populates="admitting_doctor")
 
 
 class Preset(Base):
@@ -63,12 +121,21 @@ class Preset(Base):
 class PresetRx(Base):
     preset_id = Column(Integer, ForeignKey('preset.id'))
     preset = relationship("Preset", back_populates="rxs")
-    drug_name = Column(String(250))
+
+    drug_id = Column(Integer, ForeignKey('drug.id'))
+    drug = relationship("Drug")
+
     drug_order = Column(String(250))
     active = Column(Boolean)
 
 
-def StartEngine(uri, echo):
+def StartEngine(uri, echo, client_version):
+    """
+    Initializes the database Engine and creates tables
+    as needed.
+    TODO: Check that client_version matches the database version
+    """
+
     engine = create_engine(uri, echo=echo)
 
     Base.metadata.create_all(engine)
